e4aecad2c383cbbc54b42d8594bf43133d3bbb3d
rakesh_2 <rakesh_2@rakesh-PC>
2019-04-25 09:34:44 +0530

    first commit...

---
+++ src/test/java/com/mavenDemo/tests/DemoTest.java
@@ -0,0 +1,13 @@
+package com.mavenDemo.tests;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class DemoTest 
+{
+	@Test
+	public void testA()
+	{
+		Reporter.log("hiiiii",true);
+	}
+}
